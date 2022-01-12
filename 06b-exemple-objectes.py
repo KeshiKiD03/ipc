eeaa93fdac2e24c5d4160519501367f007270dbb
isx36579183 <isx36579183@i11.informatica.escoladeltreball.org>
2022-01-12 09:49:40 +0100

    12.01.22 - IPC

--- 06b-exemple-objectes.py
+++ 06b-exemple-objectes.py
@@ -1,52 +1,32 @@
 # /usr/bin/python3
 #-*- coding: utf-8-*-
-#
-# head [-n nlin] [-f file]
-#  10 lines, file o stdin
-# -------------------------------------
-# @ edt ASIX M06 Curs 2019-2020
-# Gener 2020
-# -------------------------------------
-# Exemple de programació Objectes POO
-# -------------------------------------
 class UnixUser():
   """Classe UnixUser: prototipus de /etc/passwd login:passwd:uid:gid:gecos:home:shell"""
-  
   def __init__(self, userLine):	
     "Constructor objectes UnixUser"
     userField=userLine.split(":")
-
     self.login=userField[0]
     self.passwd=userField[1]
     self.uid=int(userField[2])
     self.gid=int(userField[3])
     self.gecos=userField[4]
     self.home=userField[5]
-    self.bash=userField[6]
- 
+    self.shell=userField[6][:-1]
+
   def show(self):
-    "Mostra les dades de l'usuari"
-    
-    print(f"login:")
+    "Mostra usuari"
+    print("login: {self.login} uid: {self.uid} gid: {self.gid} gecos: {self.gecos} home: {self.home} shell: {self.shell}")
 
-    print(self.login)
-    print(self.uid)
-    print(self.gid)
-    print
-    print
-    print
-    print
-
-  def __str__ (self):
-    "Funció to string"
-    return "%s %d %d " % (self.login, self.uid, self.gid)
+  def __str__(self):
+    "Fa string de la instancia"
+    return "%s %s %d %d %s %s %s" %(self.login, self.passwd, self.uid, self.gid, self.gecos, self.home, self.shell)
 
 print("Programa")
-
-user1=UnixUser("pere",x,1000:100::/home/pere:/bin/bash")
-
+user1=UnixUser("Pere:pere12:1000:100:pere:/home/pere:/bin/bash")
 user1.show()
-
 print(user1)
+
+
+
 
 exit(0)
