50bef32bdd174334aab19e3f8bb30b1a6dd3021c
Aaron <91669694+KeshiKiD03@users.noreply.github.com>
2022-02-07 17:18:21 +0100

    Update 13-popen-sql-injectat.py

--- 13-popen-sql-injectat.py
+++ 13-popen-sql-injectat.py
@@ -3,18 +3,28 @@
 #
 # popen-sql.py
 # -------------------------------------
-# @ edt ASIX M06 Curs 2019-2020
-# Gener 2020
+# @ edt ASIX M06 Curs 2021-2022
+# Gener 2022
 # -------------------------------------
-import sys, argparse
+# Utilitza el docker edtasixm06/postgres
+# -----------------------------------------
+#commandLocal = "psql -qtA -F ';' lab_clinic -c 'select * from pacients;'"
+#commandRemote = "psql -qtA -h i11 -U postgres -F ';' lab_clinic -c 'select * from pacients;'"
+# -------------------------------------------
+import sys
 from subprocess import Popen, PIPE
-# -------------------------------------------------------
-
-
-command = "psql -qtA -F',' -h 172.17.0.2 -U edtasixm06 training -c \"select * from clientes; \""
-pipeData = Popen(command,shell=True,stdout=PIPE)
+import argparse
+parser = argparse.ArgumentParser(description='Consulta SQL interactiva')
+parser.add_argument('sqlStatment', help='Sentència SQL a executar',metavar='sentènciaSQL')
+args = parser.parse_args()
+#----------------------------------------------------------
+cmd = "psql -qtA -F',' -h 172.17.0.2 -U postgres  training"
+pipeData = Popen(cmd, shell = True, bufsize=0, universal_newlines=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+pipeData.stdin.write("select * from oficinas;\n\q\n")
 for line in pipeData.stdout:
-  print(line.decode("utf-8"), end="")
-exit(0)
+  print(line, end="")
+sys.exit(0)
+
 
 # SOLUCIÓN
+
