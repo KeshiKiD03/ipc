8ea4674b0b9e045b17a7a06c2a9993a155f5864b
Aaron <91669694+KeshiKiD03@users.noreply.github.com>
2022-02-07 17:19:30 +0100

    Update 13b-sql-injectat-[CORREGIDA].py

--- 13b-sql-injectat-[CORREGIDA].py
+++ 13b-sql-injectat-[CORREGIDA].py
@@ -31,6 +31,46 @@ exit(0)
 
 # Lo pone en una variable.
 
+# SOLUCIÓN
+"""
+  1. Se importa SYS y desde subproceso, Popen y PIPE.
+  2. Se importa también argparse.
+  3. Se inicializa el ARGPARSE con una descripción.
+  4. Se añade un argumento que será una sentencia SQL, será posicional.
+  5. La entrada estándar, salida estándar y salida de error será PIPE.
+  6. En la entrada estándar, hacemos un WRITE hacemos una sentencia SQL. --> \n --> Salto de línea y \q es ENTER y \n salto de línea.
+  7. Se recorre con un for la salida estándar de cada línea y se printa.
+"""
+
+
+"""
+
+## NOMBRE DEL PROGRAMA + SINTAXIS
+
+**13-popen-sql-injectat.py  consulta**
+
+  Fer que la sentència sql a fer sigui un argument tot entre cometes 
+  que es passa com a argument.
+  sql injectat: perills d’injectar codi d’usuari en els programes.
+
+# ----------------------------------------------
+
+## Explicación
+
+**13-popen-sql-injectat.py  consulta**
+
+  Fer que la sentència sql a fer sigui un argument tot entre cometes 
+  que es passa com a argument.
+  sql injectat: perills d’injectar codi d’usuari en els programes.
+
+
+# ----------------------------------------------
+
+
+
+
+"""
+
 
 
 """
