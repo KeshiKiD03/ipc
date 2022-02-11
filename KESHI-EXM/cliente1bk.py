ec748cfbb84fe3d7e2b5812d86fece9c296397cf
Your Name <you@example.com>
2022-02-11 02:13:51 +0100

    Actualizado hoy 10.02.22 - IPC

--- KESHI-EXM/cliente1bk.py
+++ KESHI-EXM/cliente1bk.py
@@ -50,7 +50,7 @@ while True:
     
         data = s.recv(1024)
         
-        print(str(data))
+        print(data.decode("utf-8"))
         
         if 'cat' in rata:
 
@@ -59,7 +59,7 @@ while True:
             
         else:
 
-            print (str(data))
+            print (data.decode("utf-8"))
 
         if data[-1:] == b'\x04':
 
@@ -67,5 +67,34 @@ while True:
 
     
     
+## USAGE 
+
+## 1. python3 servidor.py 50001
+
+## 1. python3 cliente.py localhot 50001
+
+## ls
+
+## get
+
+
 sys.exit(0)
 
+
+"""
+A continuación se encuentran algunos de los comandos de FTP más comunes que podemos utilizar:
+
+    help o ? – Enumerar todos los comandos de FTP disponibles.
+    cd – Cambia el directorio en la máquina remota.
+    lcd – Cambiar el directorio en la máquina local.
+    ls – Ver los nombres de los archivos y directorios en el directorio remoto actual.
+    mkdir – Crea un nuevo directorio dentro del directorio remoto.
+    pwd – Imprime el directorio de trabajo actual en la máquina remota.
+    delete – Elimina un archivo en el directorio remoto actual.
+    rmdir- Elimina un directorio en el directorio remoto actual.
+    get – Copia un archivo del servidor remoto a la máquina local.
+    mget – Permite copiar múltiples archivos del servidor remoto a la máquina local.
+    put – Copia un archivo de la máquina local a la máquina remota.
+    mput – Copia un archivo de la máquina local a la máquina remota.
+
+"""
